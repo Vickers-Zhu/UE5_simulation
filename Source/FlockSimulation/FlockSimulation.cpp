0ce827f304ffe65f9842b33b5d2496370341370a
Vickers Zhu <vickerszhu@163.com>
2024-12-02 22:26:42 +0900

    replace the whole repo and upgrade to UE 5.4.4

--- Source/FlockSimulation/FlockSimulation.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FlockSimulation.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlockSimulation, "FlockSimulation" );
---
+++ Source/FlockSimulations/FlockSimulationsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlockSimulationsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOCKSIMULATIONS_API AFlockSimulationsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
